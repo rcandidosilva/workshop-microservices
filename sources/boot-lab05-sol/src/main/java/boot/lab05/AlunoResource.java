c24c8d10b58430544daff2d75f058238dca1c711
Rodrigo Candido da Silva <rcandidosilva@gmail.com>
2017-06-02 19:32:38 -0300

    Added boot-lab05-sol

---
+++ sources/boot-lab05-sol/src/main/java/boot/lab05/AlunoResource.java
@@ -0,0 +1,12 @@
+package boot.lab05;
+
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Resource;
+
+public class AlunoResource extends Resource<Aluno> {
+	
+	public AlunoResource(Aluno aluno, Link... links) {
+		super(aluno, links);
+	}
+	
+}
