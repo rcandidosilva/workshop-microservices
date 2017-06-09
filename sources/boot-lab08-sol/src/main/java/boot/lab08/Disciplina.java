50a8339baf05e51d076a887efd1f3dbc4f3ad6d8
Rodrigo Candido da Silva <rcandidosilva@gmail.com>
2017-06-09 11:06:33 -0300

    Added boot-lab08-sol

---
+++ sources/boot-lab08-sol/src/main/java/boot/lab08/Disciplina.java
@@ -0,0 +1,26 @@
+package boot.lab08;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Disciplina {
+
+	@Id @GeneratedValue(strategy=GenerationType.AUTO)
+	Long id;
+	String nome;
+	Integer cargaHoraria;
+	Date dataInicio;
+	
+}
