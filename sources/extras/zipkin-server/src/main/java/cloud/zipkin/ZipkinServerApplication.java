62cc2d2ab3f3c106393b8178db6ccf338adbfac3
Rodrigo Candido da Silva <rcandidosilva@gmail.com>
2017-07-31 14:14:29 -0300

    Changes at extras sources

---
+++ sources/extras/zipkin-server/src/main/java/cloud/zipkin/ZipkinServerApplication.java
@@ -0,0 +1,17 @@
+package cloud.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import zipkin.server.EnableZipkinServer;
+
+@EnableZipkinServer
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ZipkinServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinServerApplication.class, args);
+	}
+}
