d94c09ef7fca6b29a13e4679e8baa82bf328a32f
Rodrigo Candido da Silva <rcandidosilva@gmail.com>
2017-07-31 13:56:19 -0300

    Changes at extras sources

--- sources/extras/admin-server/src/main/java/cloud/admin/AdminServerApplication.java
+++ sources/extras/admin-server/src/main/java/cloud/admin/AdminServerApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
+
+@EnableAdminServer
 @EnableDiscoveryClient
 @SpringBootApplication
 public class AdminServerApplication {
