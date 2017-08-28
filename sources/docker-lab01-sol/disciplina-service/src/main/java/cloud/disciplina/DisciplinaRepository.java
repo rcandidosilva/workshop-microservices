9668408f957c8f2394f7f654e0ad4dd6f975ae4c
Rodrigo Candido da Silva <rcandidosilva@gmail.com>
2017-08-27 21:10:36 -0300

    Deleted docker labs

--- sources/docker-lab01-sol/disciplina-service/src/main/java/cloud/disciplina/DisciplinaRepository.java
+++
@@ -1,9 +0,0 @@
-package cloud.disciplina;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(path = "/disciplinas", collectionResourceRel = "disciplinas")
-public interface DisciplinaRepository extends JpaRepository<Disciplina, Long> {
-
-}
